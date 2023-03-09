d1d9b9546269e2fac4bcbe020cf8ea51ff58ad69
Joel <joelbraortiz@gmail.com>
2023-03-09 22:14:59 +0100

    Menú hamburguesa en opciones

--- app/src/main/java/com/example/proyecto1_das/OptionsActivity.java
+++ app/src/main/java/com/example/proyecto1_das/OptionsActivity.java
@@ -1,6 +1,9 @@
 package com.example.proyecto1_das;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.AlertDialog;
 import android.content.Context;
@@ -9,18 +12,21 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.proyecto1_das.dialog.OptionDialog;
 import com.example.proyecto1_das.preferences.Preferences;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-public class OptionsActivity extends AppCompatActivity implements Preferences.PrefListener {
+public class OptionsActivity extends AppCompatActivity implements Preferences.PrefListener, NavigationView.OnNavigationItemSelectedListener{
 
+    private ActionBarDrawerToggle actionBarDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +34,19 @@ public class OptionsActivity extends AppCompatActivity implements Preferences.Pr
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.fragment_pref, new Preferences())
                 .commit();
+
+
+        DrawerLayout d = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, d, R.string.nav_open, R.string.nav_close);
+
+        d.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView n = findViewById(R.id.nav_menu);
+        n.bringToFront();
+        n.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -52,5 +71,31 @@ public class OptionsActivity extends AppCompatActivity implements Preferences.Pr
     @Override
     public void changeTheme() {
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (R.id.nav_home == item.getItemId()) {
+            Intent i = new Intent(this, RoutineActivity.class);
+            startActivity(i);
+        } else if (R.id.nav_logout == item.getItemId()) {
+            boolean success = deleteFile("config.txt");
+
+            if (success) {
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        }
+        return true;
     }
 }
